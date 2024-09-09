12289916ead3b5a748af1d320cf13e4df1742a2a
nailahsalsabila27 <nailahslsb@gmail.com>
2024-09-09 11:17:27 +0700

    tess

--- MyFirstJavaGithub19.java
+++ MyFirstJavaGithub19.java
@@ -1,5 +1,6 @@
 public class MyFirstJavaGithub19 {
     public static void main(String[] args) {
         System.out.println("Hello from Github, nama saya alsa");
+        System.out.println("Hello from Github, nama saya alsa");
     }
 }
